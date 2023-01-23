93e33ae81fa387142293805c001893c581a896de
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-23 14:57:44 -0800

    Create class based header component and extend to app. Adjust app function to class.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        MY HEADER
+
+
+      </div>
+    );
+  }
+}
+
+export default Header;
