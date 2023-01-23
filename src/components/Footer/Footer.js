2ce5fd5f663a12a92304bbbd5d2af1cb54738fef
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-23 15:04:27 -0800

    Create stateless footer component, export to app.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+      <div className="App">
+        MY FOOTER
+      </div>
+    );
+};
+
+export default Footer;
+
+// Stateless component
+// use when no functionality is needed
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
     return (
       <div className="App">
         MY HEADER
-
-
       </div>
     );
   }
 }
 
 export default Header;
+
+// Stateful component
+// use when access to the state/functionality is needed
